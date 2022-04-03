23e4bc8d469795e1059acee32ce114c165d3067b
Durmuş <demirtasdurmus@gmail.com>
2022-04-03 13:11:20 +0300

    configure on macos

--- backup.sh
+++ backup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-PGPASSWORD="${1}" pg_dump -U postgres ${2} > "${3}"
+
+#PGPASSWORD="${1}" pg_dump -U postgres -p 5432 -Fc -d ${2} > "${3}"
+
+#echo Hello, World!
+
+PGPASSWORD="    " pg_dump -U postgres -p 5432 -Fc -d ${1} > "${2}"
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const cron = require('node-cron');
 const app = express()
-const port = 3000
 const { exec } = require('child_process');
 
 
@@ -11,28 +12,28 @@ const { exec } = require('child_process');
 // });
 
 // running this task everyday at 23.59
-cron.schedule('59 23 * * *', function () {
+cron.schedule('* * * * * *', function () {
     console.log('---------------------');
     console.log('Running Cron Job');
 
     // pull in credentials from .env
     const dbToBackup = process.env.DB_NAME;
-    const password = process.env.DB_PASSWORD;
+    const password = process.env.DB_PASS;
     const format = "sql";
 
     // create a custom backup file name with date info
     const date = new Date();
     const current_date = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}`;
-    const backup_file_ext = `E:\\09.Emparazon\\00.back-ups\\${dbToBackup}\\${dbToBackup}_${current_date}.${format}`;
+    const backup_file_ext = `/Users/durmusdemirtas/Documents/b.sql`;
 
     // execute node child process(exec)
-    exec(`"./backup.sh" ${password} ${dbToBackup} ${backup_file_ext}`,
+    exec("sh ./backup.sh " + dbToBackup + " " + backup_file_ext,
         (error, stdout, stderr) => {
             if (error) {
-                console.error(`exec error: ${error}`);
+                return console.error(`exec error: ${error}`);
             }
             if (stderr) {
-                console.error(`stderr: ${stderr}`);
+                return console.error(`stderr: ${stderr}`);
             }
             console.log(`Backed up your db successfully: ${stdout}`);
         })
@@ -44,6 +45,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server is awake on port ${process.env.PORT}:${process.env.NODE_ENV}`)
 })
